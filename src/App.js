4404fa0ea41f9dcd98b391f6ceb2c80708b9eec9
Dale Vivian Ross <dalevross@gmail.com>
2021-02-04 16:33:54 -0500

    Broken homepage into smaller components and finished styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 function App() { 
   return (
+  
     <div>
       <HomePage/>
     </div>
